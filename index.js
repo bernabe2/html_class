2bef5178bd1c7e1a777d4d7c6836cc17f736e1c1
alexis salaz <alexissalaz234@gmial.com>
2023-06-20 09:15:24 -0600

    prueba

---
+++ index.js
@@ -0,0 +1,17 @@
+function nameOffumction(){ 
+    var examoleTwo;
+    let eaxmpleThree;
+}
+
+const nameOffumction = function(){ 
+    var example;
+    const exampleTwo;
+    let exampleThree;
+}
+
+const nameOffunction = () => { 
+    var example;
+    const exampleTwo;
+    let example Three;
+}
+
